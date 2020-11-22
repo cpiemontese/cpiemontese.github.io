24925ce4b9e8770e9632b1960447da5ec15bc49b
Cristiano Piemontese <cristiano.piemontese@mia-platform.eu>
2020-11-22 11:28:56 +0100

    Deploy

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f72130669622eae07b38";
+/******/ 	var hotCurrentHash = "cf58af551fd69d40597c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
