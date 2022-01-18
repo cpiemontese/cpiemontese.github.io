9bc72b9d4999523cb568be17c051999935d25874
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-01-18 12:14:29 +0100

    🚀 Deploy

---
+++ _next/static/DDiC_oxyO7hz_uH5oV08Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/6728d85a-8cc77fd11121b977.js","static/chunks/1bfc9850-c40c4da79eb27507.js","static/chunks/pages/index-aeac046225de7f23.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
