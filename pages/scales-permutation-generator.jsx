d12b0f22f68210f85963d8407c533b09b6be627f
Cristiano Piemontese <cristiano.piemontese@prima.it>
2023-10-04 16:27:58 +0200

    format

--- pages/scales-permutation-generator.jsx
+++ pages/scales-permutation-generator.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import Layout from '../components/layout'
 
-function shuffle (array) {
+function shuffle(array) {
   const arrayCopy = [...array]
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]]
   }
-  return arrayCopy;
+  return arrayCopy
 }
 const notes = ['A', 'B', 'C', 'D', 'E', 'G', 'F', 'Ab', 'Bb', 'Db', 'Eb', 'Gb']
 
-export default function ScalesPermutationGenerator () {
+export default function ScalesPermutationGenerator() {
   const [init, setInit] = useState(false)
   const [currentNotes, setNotes] = useState(shuffle(notes))
 
