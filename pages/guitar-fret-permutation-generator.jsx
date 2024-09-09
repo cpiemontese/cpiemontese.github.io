5700681d2b899a4494a88f84fcd1fa838b4015a4
Cristiano Piemontese <cristiano.piemontese@google.com>
2024-09-09 22:11:16 +0200

    refactor

--- pages/guitar-fret-permutation-generator.jsx
+++ pages/guitar-fret-permutation-generator.jsx
@@ -3,41 +3,41 @@ import Permutation from '../components/permutation'
 
 const stringPerDot = [
   [1, 1],
-  [1, 2],
   [1, 3],
-  [1, 4],
   [1, 5],
-  [1, 6],
+  [1, 7],
+  [1, 9],
+  [1, 12],
   [2, 1],
-  [2, 2],
   [2, 3],
-  [2, 4],
   [2, 5],
-  [2, 6],
+  [2, 7],
+  [2, 9],
+  [2, 12],
   [3, 1],
-  [3, 2],
   [3, 3],
-  [3, 4],
   [3, 5],
-  [3, 6],
+  [3, 7],
+  [3, 9],
+  [3, 12],
   [4, 1],
-  [4, 2],
   [4, 3],
-  [4, 4],
   [4, 5],
-  [4, 6],
+  [4, 7],
+  [4, 9],
+  [4, 12],
   [5, 1],
-  [5, 2],
   [5, 3],
-  [5, 4],
   [5, 5],
-  [5, 6],
+  [5, 7],
+  [5, 9],
+  [5, 12],
   [6, 1],
-  [6, 2],
   [6, 3],
-  [6, 4],
   [6, 5],
-  [6, 6],
+  [6, 7],
+  [6, 9],
+  [6, 12],
 ]
 
 export default function GuitarStringDotPermutationGenerator() {
@@ -48,7 +48,7 @@ export default function GuitarStringDotPermutationGenerator() {
           <Permutation
             elements={stringPerDot}
             getElemFn={(elements, idx) => {
-              return `String ${elements[idx][0]} Dot ${elements[idx][1]}`
+              return `String ${elements[idx][0]} Fret ${elements[idx][1]}`
             }}
           />
         </>
