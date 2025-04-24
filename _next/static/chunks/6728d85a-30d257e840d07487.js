b3d6445cd6d593d5cd9386ae90b77d3f6486bd33
Cristiano Piemontese <cristiano.piemontese@prima.it>
2025-04-24 09:09:00 +0200

    🚀 Deploy

--- _next/static/chunks/6728d85a-30d257e840d07487.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[158],{9518:(t,e,a)=>{a.d(e,{tzA:()=>u});var L=a(6771);function u(t){return(0,L.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M23,20 L23,6 L12,15 L1,6 L1,20 L23,20 Z M12,12 L22,4 L2,4 L12,12 Z"}}]})(t)}}}]);
---
+++ _next/static/chunks/c1401c72-a96d49658e6b7147.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[852],{5269:(t,e,L)=>{L.d(e,{BLQ:()=>u});var a=L(5965);function u(t){return(0,a.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M23,20 L23,6 L12,15 L1,6 L1,20 L23,20 Z M12,12 L22,4 L2,4 L12,12 Z"}}]})(t)}}}]);
