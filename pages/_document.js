226b063ee3c355ba0e47e3baff89ac1e111b04ef
Cristiano Piemontese <cristiano.piemontese@prima.it>
2026-03-03 12:00:12 +0100

    refactor: try changing fonts

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,10 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Work+Sans&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Source+Sans+3&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body className="bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-100">
           <Main />
